58084ec50d8245e276bfe2dd8ce20d5d8b4aab82
rvazquezglez <rvazquezglez@gmail.com>
2015-11-20 18:15:19 -0800

    typos and two new tests

--- alphabet_cipher/test/test_coder.py
+++ alphabet_cipher/test/test_coder.py
@@ -4,10 +4,22 @@ import unittest
 class TestCoder(unittest.TestCase):
 
 	def test_encode(self):
-		'''can encode given a secret keyword'''
+		'''Can encode given a secret keyword'''
 		c = coder.Coder()
 		self.assertEqual("egsgqwtahuiljgs", c.encode("scones", "meetmebythetree"))
 		self.assertEqual("hmkbxebpxpmyllyrxiiqtoltfgzzv", c.encode("vigilance", "meetmeontuesdayeveningatseven"))
+
+	def test_decode(self):
+		'''Can decode a crypted message given a secret keyword'''
+		c = coder.Coder()
+		self.assertEqual("meetmebythetree", c.decode("scones", "egsgqwtahuiljgs"))
+		self.assertEqual("meetmeontuesdayeveningatseven", c.decode("vigilance", "hmkbxebpxpmyllyrxiiqtoltfgzzv"))
+
+	def test_decipher(self):
+		'''Can extract the secret keyword given an encrypted message and the original message'''
+		self.assertEqual("scones", c.decipher("hcqxqqtqljmlzhwiivgbsapaiwcenmyu", "packmyboxwithfivedozenliquorjugs"))
+		self.assertEqual("vigilance", c.decode("opkyfipmfmwcvqoklyhxywgeecpvhelzg", "thequickbrownfoxjumpsoveralazydog"))
+
 
 if __name__ == '__main__':
 	unittest.main()
